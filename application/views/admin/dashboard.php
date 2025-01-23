92e699f92e17528c0cc759d09e5744c81d4c7ae3
Wahidatuzzahro225 <126538868+Wahidatuzzahro225@users.noreply.github.com>
2025-01-23 08:38:25 +0700

    coba

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,7 @@
+<head> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+
 <?php $this->load->view('admin/template/meta') ?>
     <!--begin::App Wrapper-->
     <div class="app-wrapper">
@@ -40,19 +44,11 @@
                 <div class="small-box text-bg-primary">
                   <div class="inner">
                     <h3>150</h3>
-                    <p>New Orders</p>
+                    <p>Modul Pembelajaran</p>
                   </div>
-                  <svg
-                    class="small-box-icon"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
-                    ></path>
-                  </svg>
+                  <div class="small-box-icon">
+                    <i class="fas fa-book"></i>  <!-- Ikon buku dari Font Awesome -->
+                  </div>
                   <a
                     href="#"
                     class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
@@ -67,20 +63,12 @@
                 <!--begin::Small Box Widget 2-->
                 <div class="small-box text-bg-success">
                   <div class="inner">
-                    <h3>53<sup class="fs-5">%</sup></h3>
-                    <p>Bounce Rate</p>
+                    <h3>150</h3>
+                    <p>Modul Pembelajaran</p>
                   </div>
-                  <svg
-                    class="small-box-icon"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z"
-                    ></path>
-                  </svg>
+                  <div class="small-box-icon">
+                    <i class="fas fa-book"></i>  <!-- Ikon buku dari Font Awesome -->
+                  </div>
                   <a
                     href="#"
                     class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
@@ -95,27 +83,21 @@
                 <!--begin::Small Box Widget 3-->
                 <div class="small-box text-bg-warning">
                   <div class="inner">
-                    <h3>44</h3>
-                    <p>User Registrations</p>
+                    <h3>150</h3>
+                    <p>Modul Pembelajaran</p>
                   </div>
-                  <svg
-                    class="small-box-icon"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z"
-                    ></path>
-                  </svg>
+                  <div class="small-box-icon">
+                    <i class="fas fa-book"></i>
+                  </div>
                   <a
                     href="#"
-                    class="small-box-footer link-dark link-underline-opacity-0 link-underline-opacity-50-hover"
+                    class="small-box-footer link-light link-underline-opacity-0 link-underline-opacity-50-hover"
                   >
                     More info <i class="bi bi-link-45deg"></i>
                   </a>
                 </div>
+
+
                 <!--end::Small Box Widget 3-->
               </div>
               <!--end::Col-->
@@ -124,7 +106,7 @@
                 <div class="small-box text-bg-danger">
                   <div class="inner">
                     <h3>65</h3>
-                    <p>Unique Visitors</p>
+                    <p>COE</p>
                   </div>
                   <svg
                     class="small-box-icon"
@@ -513,7 +495,6 @@
           handle: '.card-header',
         });
       });
-
       const cardHeaders = document.querySelectorAll('.connectedSortable .card-header');
       cardHeaders.forEach((cardHeader) => {
         cardHeader.style.cursor = 'move';
@@ -530,7 +511,6 @@
       // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
       // IT'S ALL JUST JUNK FOR DEMO
       // ++++++++++++++++++++++++++++++++++++++++++
-
       const sales_chart_options = {
         series: [
           {
@@ -577,7 +557,6 @@
           },
         },
       };
-
       const sales_chart = new ApexCharts(
         document.querySelector('#revenue-chart'),
         sales_chart_options,
@@ -610,13 +589,11 @@
         GB: 320, // Great Britain
         RU: 3000, // Russia
       };
-
       // World map by jsVectorMap
       const map = new jsVectorMap({
         selector: '#world-map',
         map: 'world',
       });
-
       // Sparkline charts
       const option_sparkline1 = {
         series: [
@@ -642,10 +619,8 @@
         },
         colors: ['#DCE6EC'],
       };
-
       const sparkline1 = new ApexCharts(document.querySelector('#sparkline-1'), option_sparkline1);
       sparkline1.render();
-
       const option_sparkline2 = {
         series: [
           {
@@ -670,10 +645,8 @@
         },
         colors: ['#DCE6EC'],
       };
-
       const sparkline2 = new ApexCharts(document.querySelector('#sparkline-2'), option_sparkline2);
       sparkline2.render();
-
       const option_sparkline3 = {
         series: [
           {
@@ -698,7 +671,6 @@
         },
         colors: ['#DCE6EC'],
       };
-
       const sparkline3 = new ApexCharts(document.querySelector('#sparkline-3'), option_sparkline3);
       sparkline3.render();
     </script>
--- application/views/admin/template/sidebar.php
+++ application/views/admin/template/sidebar.php
@@ -27,34 +27,13 @@
               role="menu"
               data-accordion="false"
             >
-              <li class="nav-item menu-open">
+              <li class="nav-item">
                 <a href="#" class="nav-link active">
                   <i class="nav-icon bi bi-speedometer"></i>
                   <p>
                     Dashboard
-                    <i class="nav-arrow bi bi-chevron-right"></i>
                   </p>
                 </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="<?php echo base_url('assets/dist/pages/index.html')?>" class="nav-link active">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>ayamn</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="<?php echo base_url('assets/dist/pages/index2.html')?>" class="nav-link">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Dashboard v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="<?php echo base_url('assets/dist/pages/index3.html')?>" class="nav-link">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Dashboard v3</p>
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li class="nav-item">
                 <a href="<?php echo base_url('assets/dist/pages/generate/theme.html')?>" class="nav-link">
